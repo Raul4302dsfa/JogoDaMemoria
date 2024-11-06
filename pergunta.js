a01fdf3547a60dcd54990fa1cede6a32260dcff3
Raul4302dsfa <159590713+Raul4302dsfa@users.noreply.github.com>
2024-11-06 08:44:31 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    `<img src="img/Dumb.jpeg">`
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    `<img src="img/Pinoquio.jpeg">`
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    `<img src="img/gordo.jpeg">`
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    `<img src="img/Dumb.jpeg">`
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    `<img src="img/Pinoquio.jpeg">`
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    `<img src="img/gordo.jpeg">`
+)
